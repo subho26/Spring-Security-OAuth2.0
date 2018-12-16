cace10d0e014940411b897689c90dfcefec81d74
Shubhendu Pramanik <subho.pramanik1@gmail.com>
2018-12-16 13:26:02 +0530

    Initial commit

---
+++ spring-security-server/src/main/java/com/subho/security/springsecurityserver/SpringSecurityServerApplication.java
@@ -0,0 +1,14 @@
+package com.subho.security.springsecurityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityServerApplication.class, args);
+	}
+
+}
+
